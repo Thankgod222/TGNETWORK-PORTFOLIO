3bcde5d4df8e3fdccfd9c65a1766d1c2093d3e53
Thankgod222 <thankgodogbonna01@gmail.com>
2024-10-25 15:24:25 +0100

    dark and light mode icon inserted

--- public/app.js
+++ public/app.js
@@ -8,3 +8,14 @@ function Menu(e) {
       list.classList.remove("top-[80px]"),
       list.classList.remove("opacity-100"));
 }
+
+const timeIndicator = document.querySelector("#day_icon");
+
+function revealIcon() {
+  if (document.getElementById("day_icon").name == "moon-outline") {
+    document.getElementById("day_icon").name = "sunny-outline";
+  } else {
+    document.getElementById("day_icon").name = "moon-outline";
+  }
+}
+timeIndicator.addEventListener("click", revealIcon);
